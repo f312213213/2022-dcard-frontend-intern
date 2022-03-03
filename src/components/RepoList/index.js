0a93ad43354872844c75aa08a4011976f106ccf1
david <f312213213david@gmail.com>
2022-03-03 09:19:36 +0800

    feature: Install react-window.

--- src/components/RepoList/index.js
+++ src/components/RepoList/index.js
@@ -1,14 +1,28 @@
 import React from 'react'
+import { FixedSizeList as List } from 'react-window'
+import AutoSizer from 'react-virtualized-auto-sizer'
 
 import Repo from '../Repo'
-import RepoLoader from './components/RepoLoader'
+import { FAKE_REPOS as repos } from '../../FAKE_REPOS'
+// import RepoLoader from './components/RepoLoader'
+const AllRows = () => repos.map(repo => <Repo repo={repo} key={repo.id}/>)
 
 const RepoList = ({ repos }) => {
   return (
-      <div className={'divide-y divide-gray-300 w-full px-4'}>
-        {repos.map(repo => <Repo repo={repo} key={repo.id}/>)}
-        <RepoLoader />
-      </div>
+        <AutoSizer>
+          {({ height, width }) => (
+              <List
+                  className={'divide-y divide-gray-300 w-full px-4 min-h-screen -translate-x-1/2'}
+                  height={height}
+                  itemCount={1}
+                  itemSize={repos.length}
+                  width={width}
+              >
+                {AllRows}
+              </List>
+          )}
+        </AutoSizer>
+
   )
 }
 
--- src/pages/Trending/index.js
+++ src/pages/Trending/index.js
@@ -6,8 +6,8 @@ import { FAKE_REPOS as repos } from '../../FAKE_REPOS'
 
 const Trending = () => {
   return (
-      <div className={'w-full min-h-screen flex justify-center'}>
-        <div className={'w-full relative md:w-2/3 bg-white flex flex-col items-center rounded-2xl'}>
+      <div className={'w-full flex justify-center'}>
+        <div className={'w-full  relative md:w-2/3 bg-white flex flex-col items-center rounded-2xl'}>
           <Header header={'Trending'}/>
           <RepoList repos={repos}/>
         </div>
